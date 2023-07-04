4282825689d94ce023541ad17f097fc53d5f6cf3
DmytroHorodyskyi <107617154+DmytroHorodyskyi@users.noreply.github.com>
2023-07-04 09:14:20 +0300

    Initial Commit

---
+++ TheNewYorkTimesBooks/TheNewYorkTimesBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheNewYorkTimesBooksApp.swift
+//  TheNewYorkTimesBooks
+//
+//  Created by Dmytro Horodyskyi on 04.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TheNewYorkTimesBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
